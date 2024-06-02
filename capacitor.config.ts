6643023f66ac92d2cb1c6d2defaf11e170eebdf0
Wo-Jan <woujean@protonmail.com>
2024-06-02 18:13:14 -0500

    Reorder project structure

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'prototipocine',
-  webDir: 'www'
-};
-
-export default config;

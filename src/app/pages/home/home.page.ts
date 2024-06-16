eb7219124f492ed60cd8a916137e13a64a668a75
Mressence <cordobaguerra19@gmail.com>
2024-06-15 23:47:43 -0500

    agregue los cambios a la pagina home para que muestre la cartelera de las peliculas recientes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { 
-  IonContent, 
-  IonHeader, 
-  IonTitle, 
-  IonToolbar 
-} from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar
-  ]
+  imports: [IonicModule, CommonModule]
 })
-export class HomePage implements OnInit {
+export class HomePage {
+
+  movies = [
+    {
+      name: 'Gru Mi villano favorito 4',
+      image: 'assets/movies/movie1.jpg'
+    },
+    {
+      name: 'El bus de la vida',
+      image: 'assets/movies/movie2.jpg'
+    },
+    {
+      name: 'Fly Me to the Moon ',
+      image: 'assets/movies/movie3.jpg'
+    },
+    
+  ];
 
   constructor() { }
 
-  ngOnInit() { }
 }

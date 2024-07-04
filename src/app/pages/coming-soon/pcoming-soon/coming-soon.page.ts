dc0877fbb8b07e41218609a1802b0134140af66c
EdgarDaza <edgar.daza@utp.ac.pa>
2024-07-03 23:29:40 -0500

    added click option to coming-soon movies

--- src/app/pages/coming-soon/pcoming-soon/coming-soon.page.ts
+++ src/app/pages/coming-soon/pcoming-soon/coming-soon.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, NavController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-coming-soon',
@@ -12,7 +12,31 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class ComingSoonPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
+  
+  goComingSoon1() {
+    this.navCtrl.navigateForward("/coming-soon-1");
+  }
+
+  goComingSoon2() {
+    this.navCtrl.navigateForward("/coming-soon-2");
+  }
+
+  goComingSoon3() {
+    this.navCtrl.navigateForward("/coming-soon-3");
+  }
+
+  goComingSoon4() {
+    this.navCtrl.navigateForward("/coming-soon-4");
+  }
+
+  goComingSoon5() {
+    this.navCtrl.navigateForward("/coming-soon-5");
+  }
+
+  goComingSoon6() {
+    this.navCtrl.navigateForward("/coming-soon-6");
+  }
 
   ngOnInit() {
   }
